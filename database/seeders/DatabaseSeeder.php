678898e3d8dae93731d449db8f5e23b5ee407910
adil elkhalloufi <adilelkhalloufi@gmail.com>
2024-12-22 21:18:24 +0100

    start with filment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Adil elkhalloufi',
+            'email' => 'adil@adil.com',
+            'password' => bcrypt('adil@adil.com')
         ]);
     }
 }
