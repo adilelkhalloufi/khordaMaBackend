de44199befd0150dad77736363fc39e6ae07fdc0
adil elkhalloufi <adilelkhalloufi@gmail.com>
2024-12-31 18:11:26 +0100

    add : blog

---
+++ app/Filament/Resources/BlogResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BlogResource\Pages;
+use App\Filament\Resources\BlogResource\RelationManagers;
+use App\Models\Blog;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Concerns\Translatable;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BlogResource extends Resource
+{
+    use Translatable;
+
+    protected static ?string $model = Blog::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Landing Page';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make(Blog::COL_TITLE),
+                TextInput::make(Blog::COL_DESCRIPTION),
+                FileUpload::make(Blog::COL_IMAGE),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make(Blog::COL_TITLE),
+                TextColumn::make(Blog::COL_DESCRIPTION),
+                ImageColumn::make(Blog::COL_IMAGE),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\LocaleSwitcher::make(),
+
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBlogs::route('/'),
+            'create' => Pages\CreateBlog::route('/create'),
+            'edit' => Pages\EditBlog::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/BlogResource/Pages/CreateBlog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\BlogResource\Pages;
+
+use App\Filament\Resources\BlogResource;
+use Filament\Actions;
+use Filament\Resources\Concerns\Translatable;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBlog extends CreateRecord
+{
+    use Translatable;
+
+    protected static string $resource = BlogResource::class;
+
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\LocaleSwitcher::make(),
+            // ...
+        ];
+    }
+}
