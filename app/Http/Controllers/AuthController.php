cab7e4226d42be78ee8b94e6fceb1814900d44cf
adil elkhalloufi <adilelkhalloufi@gmail.com>
2025-01-22 16:27:35 +0100

    feat : create auth conteoler regester login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // creat efor me login and register function for api with sanctom 
+    // and return token for user
+    // and return user data
+    // and return error if user not found
+
+    public function login(Request $request) :  JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return response([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user = auth()->user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function register(Request $request) : JsonResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+ 
+    public function logout(Request $request) : JsonResponse
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
+
+    public function me(Request $request) : JsonResponse
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function updateProfile(Request $request) : JsonResponse
+    {
+        $user = auth()->user();
+
+        $user->update($request->all());
+
+        return response()->json($user);
+    }
+
+    public function updatePassword(Request $request) : JsonResponse
+    {
+        $user = auth()->user();
+
+        $user->update([
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json($user);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,26 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\FavarisController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UniteController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+ use Illuminate\Support\Facades\Route;
+ 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+//  Public routes
 Route::get('categorie', [CategorieController::class, 'index']);
 Route::get('unites', [UniteController::class, 'index']);
 Route::get('products', [ProductController::class, 'index']);
-Route::post('addToFavaris', FavarisController::class);
+
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('addToFavaris', FavarisController::class);
+});
+
 
